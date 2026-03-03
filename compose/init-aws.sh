dd0ad29a1c0e1f3c13b645c4bfe4b09b4ce57eff
Well Almeida <wellington31almeida@gmail.com>
2026-03-03 17:17:26 -0300

    raw: ajustando localstack

--- compose/init-aws.sh
+++ compose/init-aws.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+set -e
+echo "Await start localstack"
+until awslocal secretsmanager list-secrets > /dev/null 2>&1; do
+  sleep 2
+done
 awslocal secretsmanager create-secret --name sandbox/asaas/access_token --secret-string '{"token":"token_de_sandbox_asaas", "basico":"19.90", "premium":"39.90", "familia":"59.90"}'
+
+echo "Create aws local resources"
